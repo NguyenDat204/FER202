e65dd2764d249e6ca75ed2203b1861e6ff632256
NguyenDat204 <nguyentiendat29112004@gmail.com>
2025-08-11 23:54:00 +0700

    Add slot1 and slot2

---
+++ slot1/slot1-ex1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const myName = 'Nguyen Tien Dat';
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>My name is {myName}</p>
+    </div>
+  );
+}
+
+export default App;
